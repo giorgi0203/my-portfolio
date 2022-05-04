3d5a3bdb6b1065ce339e0dcbd776596c75968a64
giorgi <giorgi1997arabuli@gmail.com>
2022-05-04 23:11:33 +0300

    feat: :art: add features folder and improve configs

--- src/app/features/landing/landing.module.ts
+++ src/app/features/landing/landing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './containers/layout/layout.component';
+import { LandingRoutingModule } from './landing-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutComponent
+  ],
   imports: [
+    LandingRoutingModule,
     CommonModule
   ]
 })
