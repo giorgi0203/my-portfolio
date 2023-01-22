18ed3d689e0a771ad2fcd57ebb11cd157f276e1b
giorgi <giorgi1997arabuli@gmail.com>
2023-01-22 17:09:28 +0400

    in progress minor changes

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,4 @@
-
 export default {
     preset: 'jest-preset-angular',
-    testURL: 'https://github.com/just-jeb/angular-builders',
     setupFilesAfterEnv: [`${__dirname}/setup.ts`],
-    moduleNameMapper: {
-        '\\.(jpg|jpeg|png)$': `${__dirname}/mock-module.js`,
-    },
 };
